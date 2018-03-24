0e32341bacc4e326f2de649cd42ef3dada8f6ac2
Julian Kern <kern.julian@gmx.de>
2018-03-24 15:08:51 +0100

    update to latest mocha, steal and steal-css add "mocha-fixture", remove "test-area"

--- add-dom.js
+++ add-dom.js
@@ -5,10 +5,7 @@ if(!document.getElementById("mocha")) {
   (document.body || document.documentElement).appendChild(mocha);
 }
 if(!document.getElementById("page")) {
-  var page = document.createElement("div");
-  page.id = "page";
-  var testArea = document.createElement("div");
-  testArea.id = "test-area";
-  page.appendChild(testArea);
-  (document.body || document.documentElement).appendChild(page);
+  var fixture = document.createElement("div");
+  fixture.id = "mocha-fixture";
+  (document.body || document.documentElement).appendChild(fixture);
 }
